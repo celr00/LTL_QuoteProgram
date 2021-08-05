6bbaee83b9672621a0612ebf577f667b3c3cb61e
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2021-08-05 12:19:09 -0500

    Basic Structure

---
+++ quote.py
@@ -0,0 +1,9 @@
+"""
+Author: Carlos Enrique Lozada Rodríguez
+
+This script will contain the interation with my carrier's APIs.
+- Functions per carrier that get the information
+
+Carriers: SAIA, ABF, YRC, SMT, CFL, XPO
+"""
+
